dcdfd284d8dbc08920f6f54705cfd64b5027e85d
Abid Hussain <abidhussain3008@gmail.com>
2019-03-30 20:36:30 +0530

    add routes to get location

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,5 +10,7 @@ router.post("/hero/register", Users.registerHero);
 router.post("/driver/register", Users.registerDriver);
 router.post("/hero/location", Users.updateHeroLocation);
 router.post("/driver/location", Users.updateDriverLocation);
+router.get("/driver/location/:driver", Users.getDriverLocation);
+router.get("/hero/location/:hero", Users.getHeroLocation);
 
 module.exports = router;
