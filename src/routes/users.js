d8ab61695420126adf49c6beca58a327dd4234e4
Abid Hussain <abidhussain3008@gmail.com>
2019-03-30 19:30:03 +0530

    add routes to update location

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,5 +8,7 @@ const Users = require("../controllers/users");
 router.post("/login", Users.login);
 router.post("/hero/register", Users.registerHero);
 router.post("/driver/register", Users.registerDriver);
+router.post("/hero/location", Users.updateHeroLocation);
+router.post("/driver/location", Users.updateDriverLocation);
 
 module.exports = router;
